a41fa7e95f07cebdd03284cb3566681ead0fdfb7
Daniel <danielvieiraperei@gmail.com>
2021-08-31 21:58:20 -0300

    Criei o básico do sistema pra identificar a equação

--- calculo.js
+++ calculo.js
@@ -1,31 +1,30 @@
 const RESPOSTA = document.getElementById('resposta')
 
-const VI = parseInt(document.getElementById('vi').value
-const VF = parseInt(document.getElementById('vf').value
-const AI = parseInt(document.getElementById('ai').value
-const SI = parseInt(document.getElementById('si').value
-const SF = parseInt(document.getElementById('sf').value
-const TF = parseInt(document.getElementById('tf').value
-
 
 function torriceliVf(vi, ai, si, sf) {
 
 
 
-    return Math.sqrt(vi * vi + 2 * ai * sf)
+    return Math.sqrt((vi * vi) + (2 * ai * sf))
   }
 
-}
 
 function calcular(){
+  event.preventDefault();
 
-if (VF === 5){
-  let resposta = torriceliVf(VI, AI, SI, SF)
-}
+  let lVI = parseFloat(document.getElementById('vi').value)
+  let lVF = parseFloat(document.getElementById('vf').value)
+  let lAI = parseFloat(document.getElementById('ai').value)
+  let lSI = parseFloat(document.getElementById('si').value)
+  let lSF = parseFloat(document.getElementById('sf').value)
+  let lTF = parseFloat(document.getElementById('tf').value)
+
+
+
+if (isNaN(lVF) && isNaN(lTF)){
+  let resposta = torriceliVf(lVI, lAI, lSI, lSF)
+
 
     RESPOSTA.innerHTML = resposta
     event.preventDefault();
-
-  );
-
-}
+} }
