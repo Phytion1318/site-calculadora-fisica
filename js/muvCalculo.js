5c2a79d02d4ab0a7b20dcf591f280795f39a83f7
Paulo Henrique <phrochasilva16@gmail.com>
2021-09-01 23:49:55 -0300

    Terminei as fórmulas utilizando toricelli

--- js/muvCalculo.js
+++ js/muvCalculo.js
@@ -1,23 +1,32 @@
 const RESPOSTA = document.getElementById('resposta')
 
+const VI_INPUT = document.getElementById('vi')
+const VF_INPUT = document.getElementById('vf')
+const AI_INPUT = document.getElementById('ai')
+const SI_INPUT = document.getElementById('si')
+const SF_INPUT = document.getElementById('sf')
+const TF_INPUT = document.getElementById('tf')
+
+
+
 
 function torriceliVf(vi, ai, si, sf) {
-    return Math.sqrt((vi * vi) + (2 * ai * (sf - si)));
+  return Math.sqrt((vi * vi) + (2 * ai * (sf - si)));
   };
 
 
-function torriceliSF(vi, vf, ai){
-  return ((vf * vf) - (vi * vi)) / (2 * ai);
+function torriceliSF(vi, vf, ai, si){
+  return (((vf * vf) - (vi * vi)) / (2 * ai)) + si;
 };
 
-function texto(){
-  
-document.getElementById('texto').innerHTML = "paaa"
+function toriceleAI(vi, vf, si, sf){
+  return ((vf * vf) - (vi * vi)) / ((sf - si) * 2)
 }
 
 
 function calcular(){
   event.preventDefault();
+
 
   let lVI = parseFloat(document.getElementById('vi').value);
   let lVF = parseFloat(document.getElementById('vf').value);
@@ -27,18 +36,41 @@ function calcular(){
   let lTF = parseFloat(document.getElementById('tf').value);
 
 
-if (isNaN(lVF) && isNaN(lTF)){
-  let resposta = torriceliVf(lVI, lAI, lSI, lSF);
+  if (isNaN(lVF) && isNaN(lTF)){
+    let resposta = torriceliVf(lVI, lAI, lSI, lSF);
 
-    RESPOSTA.innerHTML = resposta
-    event.preventDefault();
-};
+      RESPOSTA.innerHTML = resposta
+      VF_INPUT.value = resposta
 
-if (isNaN(lTF) && isNaN(lSF)) {
-  let resposta = torriceliSF(lVI, lVF, lAI);
+      document.getElementById('vf_label').classList.add('text-danger')
+
+
+      event.preventDefault();
+  };
+
+  if (isNaN(lTF) && isNaN(lSF)) {
+    let resposta = torriceliSF(lVI, lVF, lAI, lSI);
+
+      RESPOSTA.innerHTML = resposta;
+      SF_INPUT.value = resposta
+
+      document.getElementById('sf_label').classList.add('text-danger')
+
+
+      event.preventDefault();
+  };
+
+  if (isNaN(lTF) && isNaN(lAI)){
+    let resposta = toriceleAI(lVI, lVF, lSI, lSF)
 
     RESPOSTA.innerHTML = resposta;
-    event.preventDefault();
-};
+    AI_INPUT.value = resposta;
 
+    document.getElementById('ai_label').classList.add('text-danger')
+
+
+    event.preventDefault();
+  };
+
+  
 };
